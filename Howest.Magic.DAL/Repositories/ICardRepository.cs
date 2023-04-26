e73ecc74e23db84096deaebcdff1ce22ebb0cbe1
Robin <robin.neyrinck@student.howest.be>
2023-04-26 16:51:23 +0200

    removed methods as they are covered in filters

--- Howest.Magic.DAL/Repositories/ICardRepository.cs
+++ Howest.Magic.DAL/Repositories/ICardRepository.cs
@@ -3,12 +3,5 @@
     public interface ICardRepository
     {
         IQueryable<Card> GetCards();
-        IQueryable<Card> GetCardsByArtist(string artistName);
-        IQueryable<Card> GetCardByName(string name);
-        IQueryable<Card> GetCardByRarity(string rarity);
-        IQueryable<Card> GetCardByColor(string color);
-        IQueryable<Card> GetCardBySet(string set);
-        IQueryable<Card> GetCardByAttribute(string attribute, int value, string comparator);
-        IQueryable<Card> GetCardByFormat(string format);
     }
 }
--- Howest.Magic.DAL/Repositories/SqlCardsRepository.cs
+++ Howest.Magic.DAL/Repositories/SqlCardsRepository.cs
@@ -10,45 +10,10 @@ public class SqlCardRepository : ICardRepository
         _db = context;
     }
 
-    public IQueryable<Card> GetCardByAttribute(string attribute, int value, string comparator)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IQueryable<Card> GetCardByColor(string color)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IQueryable<Card> GetCardByFormat(string format)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IQueryable<Card> GetCardByName(string name)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IQueryable<Card> GetCardByRarity(string rarity)
-    {
-        throw new NotImplementedException();
-    }
-
-    public IQueryable<Card> GetCardBySet(string set)
-    {
-        throw new NotImplementedException();
-    }
-
     public IQueryable<Card> GetCards()
     {
         IQueryable<Card> cards = _db.Cards
                                     .Include(c => c.Artist);
         return cards;
-    }
-
-    public IQueryable<Card> GetCardsByArtist(string artistName)
-    {
-        throw new NotImplementedException();
     }
 }
